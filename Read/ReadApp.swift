7ecdfcfbb113486539ead0bbfbc033f33130447c
o-ifeanyi <onuifeanyi95@gmail.com>
2024-01-20 11:53:33 +0100

    Initial Commit

---
+++ Read/ReadApp.swift
@@ -0,0 +1,32 @@
+//
+//  ReadApp.swift
+//  Read
+//
+//  Created by Ifeanyi Onuoha on 20/01/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ReadApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

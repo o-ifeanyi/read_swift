a3ef1c52689c1f2cdd6923c55fab031042587060
o-ifeanyi <onuifeanyi95@gmail.com>
2024-07-12 17:20:55 +0100

    update gemini model

--- Read/Core/Util/TextParser.swift
+++ Read/Core/Util/TextParser.swift
@@ -58,7 +58,7 @@ struct TextParser {
             if result.isEmpty {
                 Task {
                     AnalyticService.shared.track(event: "describe_image")
-                    let model = GenerativeModel(name: "gemini-pro-vision", apiKey: APIKey.gemini)
+                    let model = GenerativeModel(name: "gemini-1.5-flash", apiKey: APIKey.gemini)
                     
                     let response = try? await model.generateContent(Constants.describeImagePromt, image!)
                     if let text = response?.text {
